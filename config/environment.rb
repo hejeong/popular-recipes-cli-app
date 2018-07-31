68dfddc267929b69edea0427a9ddee7f30651831
Jonathan Hong <jonhong777@gmail.com>
2018-07-31 00:06:59 +0000

    Reorganize files to standard format

---
+++ config/environment.rb
@@ -0,0 +1,6 @@
+require "nokogiri"
+require 'open-uri'
+require_relative "../lib/version"
+require_relative "../lib/cli"
+require_relative "../lib/recipe"
+require_relative "../lib/scraper"
--- popular_recipes/lib/popular_recipes.rb
+++
@@ -1,9 +0,0 @@
-require "nokogiri"
-require 'open-uri'
-require_relative "popular_recipes/version"
-require_relative "popular_recipes/cli"
-require_relative "popular_recipes/recipe"
-require_relative "popular_recipes/scraper"
-module PopularRecipes
-  # Your code goes here...
-end
